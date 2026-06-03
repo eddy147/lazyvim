91c3576a5daba77691ee91cbbcaa4988051df10f
eddy147 <@eddy147@gmail.com>
2026-06-03 08:56:34 +0200

    Work around a LazyVim regression calling vim.hl.hl_op()

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,15 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   command = "silent! wall",
   nested = true,
 })
+
+-- Work around a LazyVim regression calling vim.hl.hl_op()
+pcall(vim.api.nvim_del_augroup_by_name, "lazyvim_highlight_yank")
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("lazyvim_highlight_yank", { clear = true }),
+  callback = function()
+    local hl = vim.hl or vim.highlight
+    if hl and type(hl.on_yank) == "function" then
+      hl.on_yank()
+    end
+  end,
+})
