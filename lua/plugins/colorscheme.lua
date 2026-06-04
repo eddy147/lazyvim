5964b6730580c7003028436bf03fe25b658a2984
eddy147 <eddy147@gmail.com>
2026-06-04 13:42:47 +0200

    themes etc

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -20,13 +20,27 @@ return {
   -- },
   { "NLKNguyen/papercolor-theme", lazy = false, priority = 1000 },
   {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       --colorscheme = "github_light_default",
       --colorscheme = "matugen",
       --colorscheme = "melange",
-      --colorscheme = "night-owl",
-      colorscheme = "oasis-starlight",
+      colorscheme = "nightfly",
+      -- colorscheme = "randombones",
     },
   },
 }
